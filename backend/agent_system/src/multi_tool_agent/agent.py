3ca6bd8bed6e75b8ea07a6b259e4f6add70d4d28
Po33ski <jaroslaw.popardowski@wp.pl>
2025-07-05 16:15:42 +0200

    small changes

--- backend/agent_system/src/multi_tool_agent/agent.py
+++ backend/agent_system/src/multi_tool_agent/agent.py
@@ -9,14 +9,7 @@ from . import prompt
 from ..utils.load_env_data import load_model
 from ..utils.load_env_data import load_google_api_key
 
-from .sub_agents.plan_activities.agent import plan_activities_agent
-from .sub_agents.ask_for_plan.agent import ask_for_plan_agent
 from .sub_agents.get_weather.agent import get_weather_agent
-from .tools.get_current_weather import get_current_weather
-from .tools.get_forecast import get_forecast
-from .tools.get_history_weather import get_history_weather
-from .tools.get_date import get_date
-from .tools.get_week_day import get_week_day
 
 MODEL = load_model()
 GOOGLE_API_KEY = load_google_api_key()
@@ -29,7 +22,7 @@ root_agent = Agent(
     description= prompt.ROOT_DESCRIPTION,
     global_instruction=prompt.ROOT_GLOBAL_INSTR,
     instruction=prompt.ROOT_INSTR,
-    sub_agents=[plan_activities_agent, ask_for_plan_agent, get_weather_agent], 
-    tools=[get_current_weather, get_forecast, get_history_weather, get_date, get_week_day],
+    sub_agents=[get_weather_agent], 
+    tools=[],
 )
 
